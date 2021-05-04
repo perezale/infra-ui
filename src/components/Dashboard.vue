dd2410ad1d30763dc430782a0aa3747e85d506f0
Alejandro Perez <alecio88@gmail.com>
2021-05-03 23:24:52 -0300

    add Keycloak auth

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -38,7 +38,7 @@
                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                   <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <MenuItem v-for="item in profile" :key="item" v-slot="{ active }">
-                      <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item }}</a>
+                      <a v-on:click="logout" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
                     </MenuItem>
                   </MenuItems>
                 </transition>
@@ -111,8 +111,13 @@ import Table from './Table.vue'
 import TextFilter from './TextFilter.vue';
 
 
-const navigation = ['Dashboard', 'Team', 'Projects', 'Calendar', 'Reports']
-const profile = ['Your Profile', 'Settings', 'Sign out']
+const navigation = ['Dashboard']
+const profile = [ 
+  {
+    name: 'Sign out',
+    method: 'logout'
+  }
+]
 
 export default {
   components: {
@@ -131,12 +136,19 @@ export default {
   },
   setup() {
     const open = ref(false)
-
     return {
       navigation,
       profile,
       open,
     }
   },
+  methods: {
+    logout() {
+      this.$keycloak.logout();
+      //const app = getCurrentInstance();
+      //const keycloak = app.appContext.config.globalProperties.$keycloak
+      //internalInstance.appContext.config.globalProperties.keycloak.logout();
+    }
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,56 @@ import App from './App.vue'
 import './index.css'
 import './assets/tailwind.css'
 
-createApp(App).mount('#app')
+import Keycloak from 'keycloak-js'
+
+
+const Vue = createApp(App);
+
+//Vue.use(createLogger());
+
+let initOptions = {
+  url: 'https://kc.dataviaapp.com/auth', realm: 'Verathena', clientId: 'frontend-verathena', onLoad: 'login-required'
+}
+
+let keycloak = Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    console.log("Authenticated");
+
+    console.log(keycloak);
+    keycloak.loadUserInfo().then((info)=>console.log(info));
+    console.log(keycloak.resourceAccess[initOptions.clientId].roles)
+    
+    Vue.config.globalProperties.$keycloak = keycloak
+    
+    Vue.mount('#app')
+
+    /*new Vue({
+      el: '#app',
+      render: h => h(App, { props: { keycloak: keycloak } })
+    })*/
+  }
+
+
+//Token Refresh
+  setInterval(() => {
+    keycloak.updateToken(70).then((refreshed) => {
+      if (refreshed) {
+        console.log('Token refreshed' + refreshed);
+      } else {
+        console.log('Token not refreshed, valid for '
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+      }
+    }).catch(() => {
+      console.error('Failed to refresh token');
+    });
+  }, 6000)
+
+}).catch((err) => {
+  console.log(err)
+  //const logger = useLogger();
+  //console.error("Authenticated Failed");
+});
